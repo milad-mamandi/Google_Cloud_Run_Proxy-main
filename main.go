df280fee74cce87805f994d9adec6fe880107732
Milad Mamandi <m.mamandi72@gmail.com>
2025-06-26 03:13:56 +0330

    latest

--- main.go
+++ main.go
@@ -1,30 +1,54 @@
 package main
+
 import (
-        "io"
-        "net"
-        "os"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
 )
+
 func main() {
-        listenAddr := ":" + os.Getenv("PORT")
-        targetAddr := os.Getenv("V2RAY_SERVER_IP") + ":80"
-        ln, err := net.Listen("tcp", listenAddr)
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    targetURL := os.Getenv("V2RAY_SERVER_URL") // Change to full URL
+    if targetURL == "" {
+        log.Fatal("V2RAY_SERVER_URL environment variable is required")
+    }
+
+    target, err := url.Parse(targetURL)
+    if err != nil {
+        log.Fatal("Invalid target URL:", err)
+    }
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        // Create new request to target
+        targetReq := r.Clone(r.Context())
+        targetReq.URL.Scheme = target.Scheme
+        targetReq.URL.Host = target.Host
+        targetReq.RequestURI = ""
+
+        client := &http.Client{}
+        resp, err := client.Do(targetReq)
         if err != nil {
-                return
+            http.Error(w, err.Error(), http.StatusBadGateway)
+            return
         }
-        for {
-                conn, err := ln.Accept()
-                if err != nil {
-                        continue
-                }
-                go handleConnection(conn, targetAddr)
+        defer resp.Body.Close()
+
+        // Copy headers
+        for key, values := range resp.Header {
+            for _, value := range values {
+                w.Header().Add(key, value)
+            }
         }
-}
-func handleConnection(src net.Conn, targetAddr string) {
-        dst, err := net.Dial("tcp", targetAddr)
-        if err != nil {
-                src.Close()
-                return
-        }
-        go io.Copy(dst, src)
-        go io.Copy(src, dst)
+        w.WriteHeader(resp.StatusCode)
+        io.Copy(w, resp.Body)
+    })
+
+    log.Printf("HTTP proxy server listening on port %s", port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
